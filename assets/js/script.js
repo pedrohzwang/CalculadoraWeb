21624d4a522c5c71ab0d9fac1187fa4c98505750
Pedro Henrique <51291851+pedrohzwang@users.noreply.github.com>
2020-05-12 16:10:06 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var labelResultado = document.getElementById("resultado").value;
+var labelCalculo = document.getElementById("calculo");
+
+function getValue(value){
+    labelResultado.innerHTML = value;
+};  
+
+function alterarValorLabel(){
+    labelResultado.innerHTML = "";
+};
+
